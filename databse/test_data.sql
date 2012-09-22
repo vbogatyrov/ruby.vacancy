6bc9050e314ea1141fa8eb18bd0cc3d4fd8ff685
Vladimir Bogatyrov <bogatyrov@ymail.com>
2012-09-22 13:51:10 +0300

    Rails project and some test data added

---
+++ databse/test_data.sql
@@ -0,0 +1,13 @@
+﻿insert into man (name1, contact_info, seek_employment, desired_salary) values('cat', 'cat@meow.com', false, 1000);
+insert into man (name1, contact_info, seek_employment, desired_salary) values('dog', 'dog@woof.com', true,  1500);
+insert into man (name1, contact_info, seek_employment, desired_salary) values('cat&dog', 'catdog@strange.com', true, 500);
+
+insert into skill (name) values ('C');
+insert into skill (name) values ('Java');
+insert into skill (name) values ('Erlang');
+
+insert into man_skill (man_id, skill_id) values (1, 2);
+insert into man_skill (man_id, skill_id) values (1, 3);
+insert into man_skill (man_id, skill_id) values (2, 1);
+insert into man_skill (man_id, skill_id) values (2, 2);
+insert into man_skill (man_id, skill_id) values (3, 1);
---
+++ vacancy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vacancy::Application.config.secret_token = '56502f0f7ba1892536bf7f7217d360cef6d41b39d5cf58438cf6e489c1fecd747206fe6bbad6f3e935e1c8a0285e5d776b6eb4081f915da98e39fb44f9e64fe9'
