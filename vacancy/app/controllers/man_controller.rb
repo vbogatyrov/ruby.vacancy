a036b5e0ca3a3f3283e07de7aa20f620714ca82f
Vladimir Bogatyrov <bogatyrov@ymail.com>
2012-09-23 11:53:46 +0300

    Validation for Man model; views cleanup

--- vacancy/app/controllers/man_controller.rb
+++ vacancy/app/controllers/man_controller.rb
@@ -6,8 +6,11 @@ class ManController < ApplicationController
   def create
     @man = Man.new(params[:man])
     @man.save
+    
     if @man.valid?
       redirect_to :action => 'index'
+    else 
+      render :template => "man/new"
     end
   end
 
@@ -16,16 +19,20 @@ class ManController < ApplicationController
   end
 
   def show
-    @person = Man.find(params[:id])
+    @man = Man.find(params[:id])
   end
 
   def edit
   end
   
   def update
-    man = Man.find(params[:id])
-    man.update_attributes(params[:man])
- 
-    redirect_to :action => 'index'
+    @man = Man.find(params[:id])
+    @man.update_attributes(params[:man])
+    
+    if @man.valid?
+      redirect_to :action => 'index'
+    else
+      render :template => "man/show"
+    end
   end
 end
--- vacancy/app/models/man.rb
+++ vacancy/app/models/man.rb
@@ -2,7 +2,7 @@ class Man < ActiveRecord::Base
   self.table_name = 'man'
   attr_accessible :id, 
                   :name1, 
-                  :name2, 
+                  :name2,
                   :name3, 
                   :contact_info, 
                   :seek_employment, 
@@ -14,7 +14,12 @@ class Man < ActiveRecord::Base
             :name3, :presence => true, :format => { :with => /\A[А-Яа-я\s]+\z/,
                                                     :message => "Only russian letters allowed" }
             
-  validates :contact_info, :presence => true 
+  validates :contact_info, 
+            :presence => true, :format => {# for email /^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/,
+                                           # for phone number /\(?\d{3}\)?[-\s.]?\d{3}[-\s.]\d{4}$/,
+                                           :with => /^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$|\(?\d{3}\)?[-\s.]?\d{3}[-\s.]\d{4}$/,
+                                           :message => "Email invalid"}
+   
   
   has_many :man_skill
   has_many :skills, :through =>  :man_skill
